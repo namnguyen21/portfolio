ae8a53a86ae719b25504ccbf3f79f8d8b8cebafb
Nam Nguyen <namdoannguyen@mgial.com>
2020-06-16 11:37:22 -0700

    connected to node server

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const path = require("path");
+const app = express();
 
-// app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
-})
+app.listen(process.env.PORT || 8080, () => {
+  console.log("now listening");
+});
